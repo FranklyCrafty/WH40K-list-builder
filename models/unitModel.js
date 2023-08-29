42e9dc3d4bb2fb3c84862d28026e9fd9c49072d5
FranklyCrafty <ashcrafr@gmail.com>
2023-08-28 21:13:27 -0400

    Fashcraft - 2023-08-28 - Update Unit and Weap Mdl

--- models/unitModel.js
+++ models/unitModel.js
@@ -20,25 +20,6 @@
       this.faction = unitData.faction;
       this.models = unitData.models;
     }
-    
-/*     static fromXMLData(entry) {
-      return new Unit({
-        id: entry.$.id,
-        name: entry.$.name,
-        movement: entry.characteristics[0].characteristic.find(char => char.$.name === 'M')._,
-        toughness: entry.characteristics[0].characteristic.find(char => char.$.name === 'T')._,
-        save: entry.characteristics[0].characteristic.find(char => char.$.name === 'Sv')._,
-        wounds: entry.characteristics[0].characteristic.find(char => char.$.name === 'W')._,
-        leadership: entry.characteristics[0].characteristic.find(char => char.$.name === 'Ld')._,
-        obnjective_control: entry.characteristics[0].characteristic.find(char => char.$.name === 'OC')._,
-        invulnerableSave: entry.characteristics[0].characteristic.find(char => char.$.typeName === 'Invulnerable')._,
-        abilities: entry.characteristics[0].characteristic.find(char => char.$.typeName === 'Ability')._,
-        upgrades: entry.characteristics[0].characteristic.find(char => char.$.typeName === 'Wargear')._,
-        colors: [], // This information is not present in the XML file
-        image: [], // This information is not present in the XML file
-        faction: entry.characteristics[0].characteristic.find(char => char.$.typeName === 'Faction')._
-      }); 
-    }*/
   }
   
   module.exports = Unit;
---
+++ models/weaponModel.js
@@ -0,0 +1,19 @@
+// models/weaponModel.js
+
+class Weapon {
+    constructor(weaponData) {
+      this.id = weaponData.id;
+      this.name = weaponData.name;
+      this.keywords = weaponData.keywords;
+      this.range = weaponData.range;
+      this.attacks = weaponData.attacks;
+      this.weapon_skill = weaponData.weapon_skill;
+      this.armor_penetration = weaponData.armor_penetration;
+      this.damage = weaponData.damage;
+      this.rules = weaponData.rules;
+      this.constraints = weaponData.constraints;
+      this.weapon_type = weaponData.weapon_type;
+    }
+  }
+  
+  module.exports = Weapon;
