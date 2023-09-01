28e2f23c19e593ed465d0e4a6502c551112535c1
FranklyCrafty <ashcrafr@gmail.com>
2023-08-31 22:34:35 -0400

    FC - 2023Aug31 - Parse Changes

--- parseXML.js
+++ parseXML.js
@@ -3,7 +3,12 @@ const xml2js = require('xml2js');
 
 // Read XML data from file
 const xmlData = fs.readFileSync('data/Imperium - Grey Knights.cat', 'utf-8');
+xml2js.parseString(xmlData, function(err, result) {
+  fs.writeFileSync('greyKnightsData.json', JSON.stringify(result));
+});
 
+
+/*
 // Parse XML data
 const parser = new xml2js.Parser();
 parser.parseString(xmlData, (err, result) => {
@@ -13,68 +18,74 @@ parser.parseString(xmlData, (err, result) => {
   }
 
   // Extract relevant data from the parsed result and save as JSON
-  const units = result.catalogue.sharedSelectionEntries[0].selectionEntry.map(entry => {
-    if (entry.$.type == "unit"||entry.$.type == "model"){
-      const unitData = {
-        id: entry.$.id,
-        name: entry.$.name,
-        movement: entry.profiles[0].profile[0].characteristics[0].characteristic.find(char => char.$.name === 'M')._,
-        toughness: entry.profiles[0].profile[0].characteristics[0].characteristic.find(char => char.$.name === 'T')._,
-        save: entry.profiles[0].profile[0].characteristics[0].characteristic.find(char => char.$.name === 'SV')._,
-        wounds: entry.profiles[0].profile[0].characteristics[0].characteristic.find(char => char.$.name === 'W')._,
-        leadership: entry.profiles[0].profile[0].characteristics[0].characteristic.find(char => char.$.name === 'LD')._,
-        objectiveControl: entry.profiles[0].profile[0].characteristics[0].characteristic.find(char => char.$.name === 'OC')._,
-        //abilities: entry.profiles[0].profile[1].characteristics[0].characteristic.find(char => char.$.typeName === 'Abilities')._,
-        //leaderAbilities: entry.profiles[0].profile[2].characteristics[0].characteristic.find(char => char.$.typeName === 'Abilities')._,
-        //psychicAbilities: [
-        //  entry.profiles[0].profile[3].characteristics[0].characteristic.find(char => char.$.typeName === 'Abilities')._,
-        //  entry.profiles[0].profile[4].characteristics[0].characteristic.find(char => char.$.typeName === 'Abilities')._
-        //],
-        colors: [], // This information is not present in the XML file
-        image: [],//entry.profiles[0].profile[0].characteristics[0].characteristic.find(char => char.$.name === 'image')._
-        weapons: [] // Initialize the weapons array
-      };
-
-      // Check if the entry has selectionEntryGroups and if its typeName is 'Weapon Choice'
-      if (entry.selectionEntryGroups && entry.selectionEntryGroups[0].selectionEntryGroup[0].$.name === 'Weapon Choice') {
-        // Iterate through selectionEntries within the selectionEntryGroup
-        const weaponSelections = entry.selectionEntryGroups[0].selectionEntryGroup[0].selectionEntries[0].selectionEntry.map(weaponEntry => {
-          const weaponData = {
-          // Extract weapon attributes here based on XML structure
-          id: weaponEntry.$.id,
-          name: weaponEntry.$.name,
-          //keywords: weaponData.keywords;
-          range: weaponEntry.profiles[0].profile[0].characteristics[0].characteristic.find(char => char.$.name === 'Range')._,
-          attacks: weaponEntry.profiles[0].profile[0].characteristics[0].characteristic.find(char => char.$.name === 'A')._,
-          weapon_skill: weaponEntry.profiles[0].profile[0].characteristics[0].characteristic.find(char => char.$.name === 'WS'||char.$.name === 'BS')._,
-          armor_penetration: weaponEntry.profiles[0].profile[0].characteristics[0].characteristic.find(char => char.$.name === 'AP')._,
-          damage: weaponEntry.profiles[0].profile[0].characteristics[0].characteristic.find(char => char.$.name === 'D')._,
-          //rules: weaponData.rules;
-          //constraints: weaponData.constraints;
-          //weapon_type: weaponEntry.profiles[0].profile.find(char => char.$.name === weaponEntry.$.name).typeName
-          weapon_type: weaponEntry.profiles[0].profile[0].$.typeName
-          };
-
-          return weaponData;
-        });
-        
-        unitData.weapons = weaponSelections;
-      }
-
-      return unitData;
-    }
-    else {
-      return null;
-    }
-
-  });
+  const units = result.catalogue.sharedSelectionEntries[0].selectionEntry.map(parseUnit);
 
   // Save as JSON
   fs.writeFileSync('greyKnightsData.json', JSON.stringify(units, null, 2));
 });
 
+// Function to parse a single unit/model entry
+// Function to parse a single unit/model entry
+function parseUnit(entry) {
+  if (entry.$.type === "unit" || entry.$.type === "model") {
+    const unitData = {
+      id: entry.$.id,
+      name: entry.$.name,
+      movement: findCharacteristic(entry, 'M'),
+      toughness: findCharacteristic(entry, 'T'),
+      save: findCharacteristic(entry, 'SV'),
+      wounds: findCharacteristic(entry, 'W'),
+      leadership: findCharacteristic(entry, 'LD'),
+      objectiveControl: findCharacteristic(entry, 'OC'),
+      colors: [], // This information is not present in the XML file
+      image: [], //entry.profiles[0].profile[0].characteristics[0].characteristic.find(char => char.$.name === 'image')._
+      weapons: [] // Initialize the weapons array
+    };
+
+    // Handle weapon choice if it's on the same hierarchy as profile
+    if (entry.selectionEntries) {
+      const weaponChoice = entry.selectionEntries[0].selectionEntry;//.find(item => item.selectionEntry.type === 'upgrade');
+      if (weaponChoice) {
+        unitData.weapons = weaponChoice.map(parseWeapon);
+      }
+    }
+
+    // Handle weapon choice if it's a child of the unit/model
+    if (entry.selectionEntryGroups) {
+      const weaponChoiceGroup = findWeaponChoice(entry);
+      if (weaponChoiceGroup) {
+        const weaponChoice = weaponChoiceGroup.selectionEntries[0].selectionEntry;
+        ;
+        if (weaponChoice) {
+          unitData.weapons = weaponChoice.map(parseWeapon);
+        }
+      }
+    }
+
+    return unitData;
+  } else {
+    return null;
+  }
+}
+
+
+// Function to find a characteristic value by name
+function findCharacteristic(entry, charName) {
+  console.log(charName);
+  return entry.profiles[0].profile[0].characteristics[0].characteristic.find(char => char.$.name === charName)?._;
+}
+
+// Function to find the weapon choice selection entry group
+function findWeaponChoice(entry) {
+  if (entry.selectionEntryGroups) {
+    return entry.selectionEntryGroups[0].selectionEntryGroup.find(group => group.$.name === 'Weapon Choice');
+  }
+  return null;
+}
+
 // Function to parse a weapon selection entry
 function parseWeapon(weaponEntry) {
+  console.log(weaponEntry.$.name);
   return {
     id: weaponEntry.$.id,
     name: weaponEntry.$.name,
@@ -86,3 +97,4 @@ function parseWeapon(weaponEntry) {
     weapon_type: weaponEntry.profiles[0].profile[0].$.typeName
   };
 }
+*/
