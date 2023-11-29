13de6d3f671b67407b0af0e8a20416d61b8cbc0d
FranklyCrafty <ashcrafr@gmail.com>
2023-11-28 20:32:25 -0500

    FA-2023Nov28-Parsing Modifiers and Conditions

--- services/parse10eFactionService.js
+++ services/parse10eFactionService.js
@@ -109,10 +109,11 @@ function findWeaponChoice(entry) {
  */
 function parseUnit(unitEntry) {
   const unitData = {
-    id: unitEntry.$.targetId,
+    id: unitEntry.$.id,
     name: unitEntry.$.name,
     cost: unitEntry.costs ? unitEntry.costs[0].cost[0].$.value : "",
-    //modifiers: entry.modifiers
+    costId: unitEntry.costs ? unitEntry.costs[0].cost[0].$.typeId : "",
+    modifiers: parseModifiers(unitEntry),
     movement: findCharacteristic(unitEntry, "M"),
     toughness: findCharacteristic(unitEntry, "T"),
     save: findCharacteristic(unitEntry, "SV"),
@@ -157,6 +158,7 @@ function parseModel(modelEntry) {
     selection_scope: modelEntry.constraints
       ? modelEntry.constraints[0].constraint[0].$.scope
       : "",
+    modifiers: parseModifiers(modelEntry),
   };
 
   findAndParseWeapons(modelEntry, modelData); // Find and parse weapons
@@ -198,6 +200,7 @@ function parseWeapon(weaponEntry) {
           (char) => char.$.type === "max"
         )?.$.value
       : "1",
+    modifiers: parseModifiers(weaponEntry),
   };
 }
 
@@ -234,20 +237,6 @@ function parseAdditionalRules(additionalRulesEntry) {
     name: additionalRulesEntry.$.name,
     type: additionalRulesEntry.$.type,
     rule_reference_id: additionalRulesEntry.$.targetId,
-  };
-}
-
-/**
- * Function to parse a modifier selection entry
- * @param {string} modifierEntry - parses modifiers
- * @returns {object} - The parsed modifier data.
- */
-function parseModifier(modifierEntry) {
-  console.log(modifierEntry.$.name);
-  return {
-    type: modifierEntry.$.type,
-    value: modifierEntry.$.value,
-    field: modifierEntry.$.field ?? "",
   };
 }
 
@@ -368,3 +357,169 @@ function findWeaponEntryLink(targetId) {
     return null;
   }
 }
+
+/* TODO: Add support for modifiers
+//  for modifiers and conditions, store them in a separate set of JSON objects
+//example:
+    <modifiers>
+      <modifier type="set" value="2" field="702-d447-314b-a3f6">
+        <conditions>
+          <condition type="atLeast" value="10" field="selections" scope="a037-a89c-f59f-8b1e" childId="model" shared="true"/>
+        </conditions>
+      </modifier>
+    </modifiers>
+*/ 
+function parseModifiers(entry) {
+  var modifierEntries = [];
+  if (entry.modifiers) {
+    modifiers = entry.modifiers[0].modifier;
+  
+
+    for (const modifier of modifiers) {
+      modifierEntries.push(
+        {
+          modifierType: modifier.$.type,
+          modifierValue: modifier.$.value,
+          modiferTargetField: modifier.$.field ?? "",
+          conditions: parseConditions(modifier),
+        }
+      );
+    }
+  }
+  
+  // Recursively check child entries
+  if (entry.modifierGroups) {
+    const childEntries = [...entry.modifierGroups[0].modifierGroup];
+    
+    for (const childEntry of childEntries) {
+      modifierEntries.push(parseModifiers(childEntry)); // Recursively search for weapons
+    }
+  }
+
+  return modifierEntries;
+
+}
+
+function parseConditions(entry) {
+  var conditionEntries = [];
+  if (entry.conditions) {
+    conditions = entry.conditions[0].condition;
+
+    for (const condition of conditions) {
+      console.log(condition);
+      conditionEntries.push(
+        {
+          conditionScope: condition.$.scope,
+          conditionsType: condition.$.type,
+          conditionCheckValue: condition.$.value,
+          conditionChildId: condition.$.childId ?? "",
+        }
+      );
+    }
+  }
+  
+  // Recursively check child entries
+  if (entry.conditionGroups) {
+    const childEntries = [...entry.conditionGroups[0].conditionGroup];
+    
+    for (const childEntry of childEntries) {
+      conditionEntries.push(parseConditions(childEntry)); // Recursively search for weapons
+    }
+  }
+
+  return conditionEntries;
+}
+
+// MULTIPLE MODIFIERS
+/* <categoryEntry id="d39d-c104-5d92-3d58" name="Corsairs and Travelling players" hidden="false">
+      <constraints>
+        <constraint type="max" value="-1" field="51b2-306e-1021-d207" scope="force" shared="true" id="29ed-407d-211c-bd41" includeChildSelections="true" includeChildForces="true"/>
+      </constraints>
+      <modifiers>
+        <modifier type="set" value="250" field="29ed-407d-211c-bd41">
+          <conditionGroups>
+            <conditionGroup type="and">
+              <conditions>
+                <condition type="greaterThan" value="0" field="selections" scope="force" childId="d62d-db22-4893-4bc0" shared="true" includeChildSelections="true"/>
+                <condition type="instanceOf" value="1" field="selections" scope="primary-catalogue" childId="38de-521f-1ce0-44a0" shared="true"/>
+              </conditions>
+            </conditionGroup>
+          </conditionGroups>
+        </modifier>
+        <modifier type="set" value="500" field="29ed-407d-211c-bd41">
+          <conditionGroups>
+            <conditionGroup type="and">
+              <conditions>
+                <condition type="greaterThan" value="0" field="selections" scope="force" childId="baf8-997f-e323-a090" shared="true" includeChildSelections="true"/>
+                <condition type="instanceOf" value="1" field="selections" scope="primary-catalogue" childId="38de-521f-1ce0-44a0" shared="true"/>
+              </conditions>
+            </conditionGroup>
+          </conditionGroups>
+        </modifier>
+        <modifier type="set" value="750" field="29ed-407d-211c-bd41">
+          <conditionGroups>
+            <conditionGroup type="and">
+              <conditions>
+                <condition type="greaterThan" value="0" field="selections" scope="force" childId="4204-82d0-908c-a62a" shared="true" includeChildSelections="true"/>
+                <condition type="instanceOf" value="1" field="selections" scope="primary-catalogue" childId="38de-521f-1ce0-44a0" shared="true"/>
+              </conditions>
+            </conditionGroup>
+          </conditionGroups>
+        </modifier>
+        <modifier type="set" value="true" field="hidden">
+          <conditions>
+            <condition type="notInstanceOf" value="1" field="selections" scope="primary-catalogue" childId="38de-521f-1ce0-44a0" shared="true"/>
+          </conditions>
+        </modifier>
+      </modifiers>
+    </categoryEntry> */
+
+
+// NESTED conditionGroups > Condition Group > Condition Groups > Condition Group?
+/*
+<modifiers>
+        <modifier type="set" value="true" field="hidden">
+          <conditionGroups>
+            <conditionGroup type="and">
+              <conditions>
+                <condition type="atLeast" value="1" field="forces" scope="roster" childId="1d6e-2579-8e7f-1ed4" shared="true"/>
+              </conditions>
+              <conditionGroups>
+                <conditionGroup type="or">
+                  <conditions>
+                    <condition type="atLeast" value="1" field="selections" scope="force" childId="df57-7a59-75eb-ce63" shared="true" includeChildSelections="true"/>
+                    <condition type="atLeast" value="1" field="selections" scope="force" childId="497a-263e-966a-5a15" shared="true" includeChildSelections="true"/>
+                    <condition type="atLeast" value="1" field="selections" scope="force" childId="8732-df2f-a542-805f" shared="true" includeChildSelections="true"/>
+                    <condition type="atLeast" value="1" field="selections" scope="force" childId="f48e-276e-a997-c90e" shared="true" includeChildSelections="true"/>
+                    <condition type="atLeast" value="1" field="selections" scope="force" childId="b2f5-8219-f875-917e" shared="true" includeChildSelections="true"/>
+                    <condition type="atLeast" value="1" field="selections" scope="force" childId="9262-3993-1368-be5a" shared="true" includeChildSelections="true"/>
+                    <condition type="atLeast" value="1" field="selections" scope="force" childId="828d-840a-9a67-9074" shared="true" includeChildSelections="true"/>
+                    <condition type="atLeast" value="1" field="selections" scope="force" childId="77f9-e8de-225f-9e68" shared="true" includeChildSelections="true"/>
+                    <condition type="atLeast" value="1" field="selections" scope="force" childId="c9ed-d190-419c-78d3" shared="true" includeChildSelections="true"/>
+                    <condition type="atLeast" value="1" field="selections" scope="force" childId="6f18-95b7-2662-8925" shared="true" includeChildSelections="true"/>
+                    <condition type="atLeast" value="1" field="selections" scope="force" childId="1fbf-55d9-5ca8-4e9e" shared="true" includeChildSelections="true"/>
+                    <condition type="atLeast" value="1" field="selections" scope="force" childId="e53b-7d09-eb7c-c6c7" shared="true" includeChildSelections="true"/>
+                    <condition type="atLeast" value="1" field="selections" scope="force" childId="dd79-c2fe-59d6-d2" shared="true" includeChildSelections="true"/>
+                    <condition type="atLeast" value="1" field="selections" scope="force" childId="ada5-25ce-d5d5-4ccb" shared="true" includeChildSelections="true"/>
+                    <condition type="atLeast" value="1" field="selections" scope="force" childId="ffed-8509-9bf7-820e" shared="true" includeChildSelections="true"/>
+                  </conditions>
+                </conditionGroup>
+              </conditionGroups>
+            </conditionGroup>
+          </conditionGroups>
+        </modifier>
+      </modifiers>
+*/
+
+
+//INFO LINK MODIFIERS: Always append?  If so, just auto append
+/*
+      <infoLinks>
+        <infoLink id="928c-14fb-79fb-5b2e" name="Deep Strike" hidden="false" targetId="7cb5-dd6b-dd87-ad3b" type="rule"/>
+        <infoLink id="c804-b1d7-bb3-5750" name="Deadly Demise" hidden="false" targetId="b68a-5ded-65ac-98c" type="rule">
+          <modifiers>
+            <modifier type="append" field="name" value="D3"/>
+          </modifiers>
+        </infoLink>
+      </infoLinks>
+*/
